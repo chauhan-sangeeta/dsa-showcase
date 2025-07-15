320495ec60c0a9599dda7c84e295cc3d3c61a86a
sangeeta1997 <sangeetac525@gmail.com>
2025-07-15 23:10:19 +0530

    bit manipulation basic

---
+++ src/main/java/com/dsa/showcase/bitmanipulation/basic/PowerOfTwo.java
@@ -0,0 +1,64 @@
+package com.dsa.showcase.bitmanipulation.basic;
+/*231. Power of Two
+        Solved
+Easy
+        Topics
+conpanies icon
+Companies
+Given an integer n, return true if it is a power of two. Otherwise, return false.
+
+An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+
+
+        Example 1:
+
+Input: n = 1
+Output: true
+Explanation: 20 = 1
+Example 2:
+
+Input: n = 16
+Output: true
+Explanation: 24 = 16
+Example 3:
+
+Input: n = 3
+Output: false
+
+
+Constraints:
+
+        -231 <= n <= 231 - 1
+
+
+Follow up: Could you solve it without loops/recursion?*/
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        PowerOfTwo powerOfTwo = new PowerOfTwo();
+        int n = 16; // Example input
+        System.out.println("Is " + n + " a power of two? " + powerOfTwo.isPowerOfTwo(n));
+        System.out.println("Is " + n + " a power of two (constant time)? " + powerOfTwo.isPowerOfTwoInConstantTime(n));
+    }
+
+    //time complexity: O(log n)
+    public boolean isPowerOfTwo(int n) {
+        boolean isPower = false;
+        while (n >= 1) {
+            if ((n & 1) == 1) {
+                if (isPower)
+                    return false;
+                else {
+                    isPower = true;
+                }
+            }
+
+            n = n >> 1;
+        }
+        return isPower;
+    }
+
+    public boolean isPowerOfTwoInConstantTime(int n) {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+}
---
+++ src/main/java/com/dsa/showcase/bitmanipulation/basic/SetRightMostUnsetBit.java
@@ -0,0 +1,29 @@
+package com.dsa.showcase.bitmanipulation.basic;
+
+// User function Template for Java
+public class SetRightMostUnsetBit {
+    public static void main(String[] args) {
+        SetRightMostUnsetBit setRightMostUnsetBit = new SetRightMostUnsetBit();
+        int n = 10; // Example input
+        System.out.println("Original number: " + n);
+        System.out.println("Number after setting rightmost unset bit: " + setRightMostUnsetBit.setBit(n));
+        System.out.println("Number after setting rightmost unset bit using constant time: " + setRightMostUnsetBit.setBitUsingConstantTime(n));
+    }
+
+    //time complexity: O(log n)
+    int setBit(int n) {
+        // code here
+        int mask = 1;
+        int original = n;
+        while ((n & 1) == 1) {
+            n = n >> 1;
+            mask = mask << 1;
+        }
+        return mask | original;
+    }
+
+    //time complexity: O(1)
+    int setBitUsingConstantTime(int n) {
+        return n | (~n & (n + 1));
+    }
+}
